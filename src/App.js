a79315265150e9c5651ee88d88fda4b10df3e77c
Raimond Fowler <raifowler@gmail.com>
2018-08-17 12:24:02 -0700

    Created Navbar and started on Dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import Dashboard from "./components/layout/Dashboard";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Bloc Chat</h1>
-      </div>
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+            <div className="container-fluid">
+              <Link to="/" className="navbar-brand">
+                Bloc Chat
+              </Link>
+            </div>
+          </nav>
+          <main>
+            <div className="container-fluid">
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+              </Switch>
+            </div>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
